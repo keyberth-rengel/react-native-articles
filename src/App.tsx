99e6278c77600c77cb215ce1b01d8694590e5a70
keyberth rengel <keyberthdev@gmail.com>
2022-08-01 15:30:20 -0500

    config[ios] add splash in IOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
+import RNBootSplash from 'react-native-bootsplash';
 
 //navigator
 import {Home} from './navigator';
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({fade: true});
+      console.log('Bootsplash has been hidden successfully');
+    });
+  }, []);
   return <Home />;
 };
 
