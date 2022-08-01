e8b840c78904a2414170bebda42e8f5a0220f9af
keyberth rengel <keyberthdev@gmail.com>
2022-08-01 14:36:26 -0500

    refactor[src] add App.tsx in src

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+//navigator
+import {Home} from './navigator';
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
