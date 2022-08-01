7896c146f95b31dd6c050aaa29a3557760fed21b
keyberth rengel <keyberthdev@gmail.com>
2022-08-01 17:10:52 -0500

    config[android] add splash in Android

--- android/app/src/main/java/com/articles/MainActivity.java
+++ android/app/src/main/java/com/articles/MainActivity.java
@@ -2,6 +2,9 @@ package com.articles;
 
 import com.facebook.react.ReactActivity;
 
+import com.facebook.react.ReactActivityDelegate; // <- add this necessary import
+import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -11,5 +14,17 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Articles";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+
+      @Override
+      protected void loadApp(String appKey) {
+        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+        super.loadApp(appKey);
+      }
+    };
   }
 }
